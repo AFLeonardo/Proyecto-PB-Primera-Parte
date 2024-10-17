e52d187901da7cba2cd7b1a148d73fa3e7c18014
Leonardo <alfonsorruiz012@gmail.com>
2024-10-17 17:23:49 -0600

    Se agregaron las estructuras. Se agregaron las fucniones para los menus, solo que aun no funcionan. MENU DE ARTICULOS FUNCIONANDO.

--- main.c
+++ main.c
@@ -3,9 +3,17 @@
 #include <stdbool.h>
 #include <string.h>
 
-// FUNCIONES
+// MENUS
 int menu_principal();
 void menu_articulos();
+void menu_insumos();
+void menu_mercados();
+void menu_empleados();
+void menu_proveedores();
+void menu_control_ventas();
+void menu_control_compras();
+void menu_control_inventario();
+void menu_reportes();
 
 // ESTRUCTURAS
 struct Articulos
@@ -20,7 +28,68 @@ struct Articulos
     int Inventario;
     float Precio_venta;
 };
-
+struct Insumo 
+{
+    int clave_insumo;
+    char descripcion[100];
+    int punto_reorden;
+    int inventario;
+    int clave_proveedor[10]; // Hasta 10 claves de proveedores
+    float precio_compra;
+};
+struct Proveedor 
+{
+    int numero_proveedor;
+    char nombre_completo[100];
+    char RFC[14];
+    char correo[100];
+    float descuento;
+    int año_nacimiento;
+    int mes_nacimiento;
+    int dia_nacimiento;
+    char direccion[200];  // Calle, número, colonia, municipio y estado
+    int articulos_produce[10]; // Claves de los artículos que produce
+};
+struct Empleado 
+{
+    int numero_empleado;
+    char nombre_completo[100];
+    char RFC[14];
+    char correo[100];
+    float comision;
+    int año_contratacion;
+    int mes_contratacion;
+    int dia_contratacion;
+    char direccion[200];
+};
+struct Mercado 
+{
+    int clave_mercado;
+    char nombre_completo[100];
+    char RFC[14];
+    char correo[100];
+    float descuento;
+    int año_nacimiento;
+    int mes_nacimiento;
+    int dia_nacimiento;
+    char direccion[200];
+};
+struct Venta 
+{
+    int numero_mercado;
+    int numero_articulo;
+    int cantidad;
+    float precio_total;
+    int numero_empleado;
+    int factura;
+};
+struct Compra 
+{
+    int numero_proveedor;
+    int numero_insumo;
+    int cantidad;
+    float precio_total;
+};
 
 int main()
 {
@@ -111,7 +180,7 @@ void menu_articulos()
     char agregar = 'S';
     struct Articulos x_articulo;
 
-    while (agregar != 'N' || agregar != 'n')
+    while (agregar != 'N' && agregar != 'n')
     {
         printf("\n\%20s", "ARTICULOS\n");
         //Validacion clave entre 1-1000
@@ -124,7 +193,7 @@ void menu_articulos()
             
         } while (x_articulo.Clave_articulo > 1000 || x_articulo.Clave_articulo < 1);
 
-        /* //Validacion Descripcion Mimino 10 caracteres.
+        //Validacion Descripcion Mimino 10 caracteres.
         do
         {
             printf("2) Descripcion: ");
@@ -136,14 +205,13 @@ void menu_articulos()
         } while (strlen(x_articulo.Descripcion) < 10);
         
         // Las temporadas se manejan de acuerdo a los meses. 1-12
-        // Validar a que la temporada ingresa sea entre 1-12.
         do
         {
             printf("3) Temporada de siembra: ");
             scanf("%d", &x_articulo.Temp_siembra);
 
             if(x_articulo.Temp_siembra < 1 || x_articulo.Temp_siembra > 12)
-                printf("Temporada no valida.\nValores admitidos: 1 a 12");
+                printf("Temporada no valida.\nValores admitidos: 1 a 12\n");
         } while (x_articulo.Temp_siembra < 1 || x_articulo.Temp_siembra > 12);
         
         do
@@ -152,7 +220,7 @@ void menu_articulos()
             scanf("%d", &x_articulo.Temp_cosecha);
 
             if(x_articulo.Temp_cosecha < 1 || x_articulo.Temp_cosecha > 12)
-                printf("Temporada no valida.\nValores admitidos: 1 a 12");
+                printf("Temporada no valida.\nValores admitidos: 1 a 12\n");
         } while (x_articulo.Temp_cosecha < 1 || x_articulo.Temp_cosecha > 12);
         
         do
@@ -181,7 +249,7 @@ void menu_articulos()
 
             if(x_articulo.Clave_mercados < 0)
                 printf("Valor invalido.\nMinimo 0.");
-        } while (x_articulo.Clave_mercados < 0); */
+        } while (x_articulo.Clave_mercados < 0);
 
         // GUARDAR LOS DATOS EN UN ARCHIVO DIRECTO.
 
@@ -190,13 +258,11 @@ void menu_articulos()
         {
             printf("Agregar otro articulo (S/N): ");
             fflush(stdin);
-            scanf("%c", &agregar);
+            scanf(" %c", &agregar);
             if (agregar != 'S' && agregar != 's' && agregar != 'N' && agregar != 'n')
                 printf("Valor no valido.\nSolo se permite S o N.\n");
             
         } while (agregar != 'S' && agregar != 's' && agregar != 'N' && agregar != 'n');
         
     }
-    
-    printf("%d %s %d %d %d %d %f %d %f",x_articulo.Clave_articulo, x_articulo.Descripcion, x_articulo.Temp_siembra, x_articulo.Temp_cosecha, x_articulo.Inventario, x_articulo.Precio_venta, x_articulo.Clave_mercados);
 }
