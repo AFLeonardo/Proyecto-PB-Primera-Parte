49d8afad36db4dbc668d151fa6ece0223e7f4d7a
Leonardo <alfonsorruiz012@gmail.com>
2024-11-08 19:41:25 -0600

    Cambios menores

--- VER COMO SE ESCRIBEN/Lectura_Insumos.c
+++ VER COMO SE ESCRIBEN/Lectura_Insumos.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Insumo {
+struct Fechas
+{
+    int anio;
+    int mes;
+    int dia;
+};
+
+struct Direcciones
+{
+    char calle[50];
+    int numero;
+    char colonia[50];
+    char municipio[50];
+    char estado[50];
+};
+
+struct Insumo
+{
     int clave_insumo;
     char descripcion[100];
     int punto_reorden;
     int inventario;
-    int clave_proveedor[10]; // Suponiendo que hay un máximo de 10 proveedores
-    float precio_compra[10]; // Suponiendo que hay un máximo de 10 proveedores
+    int clave_proveedor[10]; // Hasta 10 claves de proveedores
+    float precio_compra[10];
 };
 
-struct Proveedor {
+struct Proveedor
+{
     int numero_proveedor;
-    // Otros campos del proveedor
+    char nombre_completo[100];
+    char RFC[14];
+    char correo[100];
+    float descuento;
+    struct Fechas fecha;
+    struct Direcciones direccion;
+    int articulos_produce[10]; // Claves de los artículos que produce
 };
 
 // Función para mostrar la información de los insumos
@@ -31,7 +55,7 @@ void mostrar_insumos(FILE *insumosf, FILE *proveedorlocal) {
     printf("\n--- Lista de Insumos ---\n");
     while (fread(&insumos, sizeof(struct Insumo), 1, insumosf) == 1) {
         printf("Clave del Insumo: %d\n", insumos.clave_insumo);
-        printf("Descripción: %s\n", insumos.descripcion);
+        printf("Descripcion: %s\n", insumos.descripcion);
         printf("Punto de Reorden: %d\n", insumos.punto_reorden);
         printf("Inventario: %d\n", insumos.inventario);
         printf("Proveedores:\n");
